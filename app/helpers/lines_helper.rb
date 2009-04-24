afa8a795515ab716e7ede8f2bb5f8a50fc6cc2d6
Mike Schwab <mschwab@Macintosh.local>
2009-04-24 01:21:01 -0500

    lines can be made public

--- app/helpers/lines_helper.rb
+++ app/helpers/lines_helper.rb
@@ -1,2 +1,11 @@
 module LinesHelper
+  def publicize_link line
+    if current_user == line.user
+      if line.public?
+        link_to 'make private', line_path(line, :line => {:public => false}), :method => :put
+      else
+        link_to 'make public', line_path(line, :line => {:public => true}), :method => :put
+      end
+    end
+  end
 end
