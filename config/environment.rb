05ef6ab5887c492fc483649f8c1ae5dd47809f95
Mike Schwab <mschwab@Macintosh.local>
2009-04-30 01:53:18 -0500

    unpacked gems fingers crossed for dreamhost

--- config/environment.rb
+++ config/environment.rb
@@ -23,6 +23,7 @@ Rails::Initializer.run do |config|
   config.gem 'andand'
   config.gem "josevalim-rails-footnotes",  :lib => "rails-footnotes", :source => "http://gems.github.com"
   config.gem 'seamusabshere-scriptaculous_slider', :lib => 'scriptaculous_slider', :source => "http://gems.github.com"
+  config.gem 'rack'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
