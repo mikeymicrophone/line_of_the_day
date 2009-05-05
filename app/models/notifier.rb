c7ef04636272ae858d6b846f1b86d8c7c60eb499
Mike Schwab <mike.schwab@gmail.com>
2009-05-05 00:45:42 -0500

    password reset tweaks

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -3,7 +3,7 @@ class Notifier < ActionMailer::Base
   
   def password_reset_instructions(user)
     subject       "Password Reset Instructions"
-    from          "Binary Logic Notifier <noreply@binarylogic.com>"
+    from          "The Sauce <mike@lineoftheday.com>"
     recipients    user.email
     sent_on       Time.now
     body          :edit_password_reset_url => edit_password_reset_url(user.perishable_token)
