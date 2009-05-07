bc092020e0a0144a3fd0ffc989abf3cf328acddb
Mike Schwab <mike.schwab@gmail.com>
2009-05-07 15:47:32 -0500

    plugin line out of env

--- config/environment.rb
+++ config/environment.rb
@@ -26,7 +26,7 @@ Rails::Initializer.run do |config|
   config.gem 'rack'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
-  config.plugins = [ :all ] - [ :xss_terminate ]
+  # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
   # Skip frameworks you're not going to use. To use Rails without a database,
   # you must remove the Active Record framework.
