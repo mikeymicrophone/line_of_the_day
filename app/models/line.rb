d5de23d642f4b3e04206c0797a73a4fb10a4c014
Mike Schwab <mike.schwab@gmail.com>
2009-10-03 18:42:58 -0400

    lines in reverse chronological order

--- app/models/line.rb
+++ app/models/line.rb
@@ -5,7 +5,7 @@ class Line < ActiveRecord::Base
   has_many :publications
   has_many :groups, :through => :publications
   has_many :approaches
-  named_scope :public, :conditions => {:public => true}
+  named_scope :public, :conditions => {:public => true}, :order => 'created_at desc'
   named_scope :novel_to, lambda { |artist|
     {:conditions => ['lines.user_id is not ?', artist.id]}
   }
