66601ea1164a385393d381a426f32db5a92c6296
Mike Schwab <mike.schwab@gmail.com>
2009-10-28 11:31:13 -0400

    can accept or reject coaching request

--- app/helpers/guidances_helper.rb
+++ app/helpers/guidances_helper.rb
@@ -1,2 +1,16 @@
 module GuidancesHelper
+  
+  def acceptance_links guidance
+    case guidance.status
+    when 'requested'
+      if current_user == guidance.coach
+        link_to_remote('accept', :url => guidance_path(guidance, :guidance => {:status => 'accepted'}), :method => :put) + ' ' +
+        link_to_remote('reject', :url => guidance_path(guidance, :guidance => {:status => 'rejected'}), :method => :put)
+      else
+        ''
+      end      
+    else
+      ''
+    end
+  end
 end
