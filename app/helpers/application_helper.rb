66c1df1c2848b0f99aabcf4abe5a6023963d4aac
Mike Schwab <mike.schwab@gmail.com>
2010-06-14 19:54:22 -0400

    slider updates page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,13 +19,13 @@ module ApplicationHelper
     vote = thevote.opinion
     type = target.class.name.downcase
     "<div class=\"vote\" id=\"#{target.dom_id('vote')}\">
+       <div class=\"current_value\" id=\"#{target.dom_id('current_value')}\"></div>
        <div class=\"track\" id=\"#{target.dom_id('track')}\">
          <div class=\"handle\" id=\"#{target.dom_id('handle')}\"></div>
 		   </div>
        <div class=\"fill\" id=\"#{target.dom_id('fill')}\"></div>
 		   <div class=\"endcap_left\"></div>
 		   <div class=\"endcap_right\"></div>
-		   <div class=\"current_value\" id=\"#{target.dom_id('current_value')}\"></div>
 		 </div>
 		 <script type=\"text/javascript\">vote_slider('#{type}', #{target.id}, #{vote});</script>"
   end
