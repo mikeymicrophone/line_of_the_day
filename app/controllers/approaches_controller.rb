68ae8a8db9dee3b8b6767998fded03b284774736
Mike Schwab <mike.schwab@gmail.com>
2010-06-14 15:04:33 -0400

    -v

--- app/controllers/approaches_controller.rb
+++ app/controllers/approaches_controller.rb
@@ -4,8 +4,7 @@ class ApproachesController < ApplicationController
   auto_complete_for :result, :name
   skip_before_filter :verify_authenticity_token, :only => [:auto_complete_for_line_phrasing, :auto_complete_for_scene_name, :auto_complete_for_result_name]
   before_filter :require_user
-  # GET /approaches
-  # GET /approaches.xml
+
   def index
     if @line = Line.find_by_id(params[:line_id])
       @approaches = @line.approaches
@@ -14,40 +13,34 @@ class ApproachesController < ApplicationController
     end
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html
       format.xml  { render :xml => @approaches }
     end
   end
 
-  # GET /approaches/1
-  # GET /approaches/1.xml
   def show
     @approach = Approach.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html
       format.xml  { render :xml => @approach }
     end
   end
 
-  # GET /approaches/new
-  # GET /approaches/new.xml
   def new
     @approach = Approach.new :line_id => params[:line_id], :scene_id => params[:scene_id], :result_id => params[:result_id]
 
     respond_to do |format|
-      format.html # new.html.erb
+      format.html
       format.xml  { render :xml => @approach }
     end
   end
 
-  # GET /approaches/1/edit
   def edit
-    @approach = Approach.find(params[:id])
+    @approach = Approach.find params[:id]
+    redirect_to @approach unless current_user == @approach.user
   end
 
-  # POST /approaches
-  # POST /approaches.xml
   def create
     params[:approach][:user] = current_user
     @approach = Approach.new(params[:approach])
@@ -58,40 +51,36 @@ class ApproachesController < ApplicationController
     respond_to do |format|
       if @approach.save
         flash[:notice] = 'Approach was successfully created.'
-        format.html { redirect_to(@approach) }
+        format.html { redirect_to @approach }
         format.xml  { render :xml => @approach, :status => :created, :location => @approach }
       else
-        format.html { render :action => "new" }
+        format.html { render :action => 'new' }
         format.xml  { render :xml => @approach.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # PUT /approaches/1
-  # PUT /approaches/1.xml
   def update
     @approach = Approach.find(params[:id])
 
     respond_to do |format|
       if @approach.update_attributes(params[:approach])
         flash[:notice] = 'Approach was successfully updated.'
-        format.html { redirect_to(@approach) }
+        format.html { redirect_to @approach }
         format.xml  { head :ok }
       else
-        format.html { render :action => "edit" }
+        format.html { render :action => 'edit' }
         format.xml  { render :xml => @approach.errors, :status => :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /approaches/1
-  # DELETE /approaches/1.xml
   def destroy
     @approach = Approach.find(params[:id])
     @approach.destroy
 
     respond_to do |format|
-      format.html { redirect_to(approaches_url) }
+      format.html { redirect_to approaches_url }
       format.xml  { head :ok }
     end
   end
