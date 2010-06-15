fb949240c9a06ea53f011aa54e3d58bfb8a4f829
Mike Schwab <mike.schwab@gmail.com>
2010-06-15 11:12:01 -0400

    fixed publication ajax

--- app/helpers/lines_helper.rb
+++ app/helpers/lines_helper.rb
@@ -2,9 +2,9 @@ module LinesHelper
   def publicize_link line
     if current_user == line.user && current_user
       if line.public?
-        link_to_remote 'make private', :url => line_path(line, :line => {:public => false}), :method => :put, :html => {:class => 'publication', :id => dom_id(line, 'publicize')}, :update => dom_id(line, 'publicize')
+        link_to_remote 'make private', :url => line_path(line, :line => {:public => false}), :method => :put, :html => {:class => 'publication'}, :update => dom_id(line, 'publicize')
       else
-        link_to_remote 'make public', :url => line_path(line, :line => {:public => true}), :method => :put, :html => {:class => 'publication', :id => dom_id(line, 'publicize')}, :update => dom_id(line, 'publicize')
+        link_to_remote 'make public', :url => line_path(line, :line => {:public => true}), :method => :put, :html => {:class => 'publication'}, :update => dom_id(line, 'publicize')
       end
     end
   end
