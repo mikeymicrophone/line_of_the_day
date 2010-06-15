03b31cdbaab586e8118625b2bdf8451490ac9a7e
Mike Schwab <mike.schwab@gmail.com>
2010-06-15 13:49:12 -0400

    reply to comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :target, :polymorphic => true
   belongs_to :user
+  has_many :comments, :as => :target
   named_scope :main, :conditions => {:target_id => nil}, :order => 'created_at desc', :limit => 88
   validates_presence_of :text
 end
