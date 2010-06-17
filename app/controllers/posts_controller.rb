db931640992899a871ee27b994248d098cf23398
Mike Schwab <mike.schwab@gmail.com>
2010-06-17 13:08:03 -0400

    sort posts by rating

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,17 @@
 class PostsController < ApplicationController
   def index
     @posts = if params[:blog_id]
-      Blog.find(params[:blog_id]).posts
+      if params[:sort] == 'rating'
+        Blog.find(params[:blog_id]).posts.sort_by { |p| p.average_rating }.reverse
+      else
+        Blog.find(params[:blog_id]).posts
+      end
     else
-      Post
+      if params[:sort] == 'rating'
+        Post.all.sort_by { |p| p.average_rating }.reverse
+      else
+        Post
+      end
     end.paginate :page => params[:page]
   end
   
