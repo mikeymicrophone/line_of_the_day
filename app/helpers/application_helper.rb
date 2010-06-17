76989d58ff70dfa9b790b09144cd08891f4f8c1f
Mike Schwab <mike.schwab@gmail.com>
2010-06-17 16:32:46 -0400

    google analytics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,5 +37,21 @@ module ApplicationHelper
   
   def add_to_list target
     link_to '+', send("new_#{target.class.name.downcase}_list_item_path", target), :title => 'add to a list' if current_user
-  end  
+  end
+  
+  def google_analytics
+    %Q!<script type="text/javascript">
+
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', 'UA-333706-3']);
+      _gaq.push(['_trackPageview']);
+
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
+
+    </script>!
+  end
 end
