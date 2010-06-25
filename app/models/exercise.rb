afbd6c7d43e68cd3768add54cfe1ee93628c0115
Mike Schwab <mike.schwab@gmail.com>
2010-06-25 13:18:56 -0400

    random exercises

--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -3,4 +3,7 @@ class Exercise < ActiveRecord::Base
   include Ratable
   belongs_to :user
   has_many :comments, :as => :target
+  
+  extend Random
+  named_scope :randomized, :order => db_random
 end
