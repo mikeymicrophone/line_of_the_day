72ea818ca7c2602f635edc1f68f8326e3f18e278
Mike Schwab <mike.schwab@gmail.com>
2010-07-10 15:49:57 -0400

    search most models

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,14 @@
 class SearchController < ApplicationController
   def index
-    @lines = Line.find(:all, :conditions => ["phrasing like ?", "%#{params[:query]}%"])
+    query = "%#{params[:query]}%"
+    @lines = Line.find :all, :conditions => ["phrasing like ?", query]
+    @tips = Tip.find :all, :conditions => ["advice like ?", query]
+    @exercises = Exercise.find :all, :conditions => ["name like ? or description like ?", query, query]
+    @stories = Story.find :all, :conditions => ["name like ? or telling like ?", query, query]
+    @posts = Post.find :all, :conditions => ["name like ? or content like ?", query, query]
+    @questions = Question.find :all, :conditions => ["name like ?", query]
+    @articles = Article.find :all, :conditions => ["title like ? or source like ?", query, query]
+    @quotes = Quote.find :all, :conditions => ["text like ?", query]
+    @tags = Concept.find :all, :conditions => ["name like ?", query]
   end
 end
