7378c5ab5d7298acb67c36159af202c0dd6500c1
Mike Schwab <mike.schwab@gmail.com>
2010-07-15 01:15:46 -0400

    migration ready for normal use

--- db/migrate/20100714000225_rename_description_on_exercise.rb
+++ db/migrate/20100714000225_rename_description_on_exercise.rb
@@ -1,11 +1,11 @@
 class RenameDescriptionOnExercise < ActiveRecord::Migration
   def self.up
-    # rename_column :exercises, :description, :instruction
+    rename_column :exercises, :description, :instruction
     rename_column :exercises, :name, :moniker
   end
 
   def self.down
-    # rename_column :exercises, :instruction, :description
-    # rename_column :exercises, :moniker, :name
+    rename_column :exercises, :instruction, :description
+    rename_column :exercises, :moniker, :name
   end
 end
