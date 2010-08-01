c613c81fd933c6b5adc582d0be1ccbaf2e19ee54
Mike Schwab <mike.schwab@gmail.com>
2010-08-01 16:25:28 -0400

    admin can make lines private

--- app/helpers/lines_helper.rb
+++ app/helpers/lines_helper.rb
@@ -1,6 +1,6 @@
 module LinesHelper
   def publicize_link line
-    if current_user == line.user && current_user
+    if current_user && (current_user == line.user || current_user.admin?)
       if line.public?
         link_to_remote 'make private', :url => line_path(line, :line => {:public => false}), :method => :put, :html => {:class => 'publication'}, :update => dom_id(line, 'publicize')
       else
--- app/models/tip.rb
+++ app/models/tip.rb
@@ -4,6 +4,7 @@ class Tip < ActiveRecord::Base
   include Commendable
   belongs_to :user
   has_many :goals, :as => :objective
+  validates_presence_of :advice
   
   extend Random
   named_scope :randomized, :order => db_random
