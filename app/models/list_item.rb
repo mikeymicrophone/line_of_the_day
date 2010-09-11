9de249013a18cb3c2884d77863a9f2f8f2f56788
Mike Schwab <mike.schwab@gmail.com>
2010-09-11 10:11:29 -0400

    unique list item validation fixed

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -6,7 +6,7 @@ class ListItem < ActiveRecord::Base
   belongs_to :item, :polymorphic => true
   named_scope :public_to, lambda { |u| {:conditions => ['private is null or user_id = ?', u.andand.id]} }
   acts_as_list :scope => :list
-  validates_uniqueness_of :item, :scope => :list
+  validates_uniqueness_of :item_id, :scope => [:item_type, :list_id]
   
   extend Random
   named_scope :randomized, :order => db_random
